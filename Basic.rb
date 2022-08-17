4c36863a10579e96d823b15b234a4dd9f9cc9862
Deepak Rawat <52197650+DeepakRawat24@users.noreply.github.com>
2022-08-17 17:39:58 +0530

    Update Basic.rb

--- Basic.rb
+++ Basic.rb
@@ -1,76 +1,5 @@
-address = [1,2,3,4,5,6,7,8,9]
-p address
-p address[5]
-new_address = address.reverse!
-p new_address
-
-# print "deepak \n"
-# puts "deepak"
-# p "deepak"
-
-# #method
-# def method_name
-#   puts "say hello"
-# end
-# method_name
 
 
-# def method_take_arg(hello)
-#   puts hello
-# end  
-
-# method_take_arg "hello ji"
-
-# #string concatenation
-# str1 = "deepak"
-# str2 = 'rawat'
-# p str1 + str2
-
-# #string interpolation
-# puts "my name is #{str1} singh #{str2}"
-
-# #common methods
-# full_name = "deepak singh"
-# p full_name.capitalize
-
-# p full_name.empty?
-# p ''.empty?
-
-# sentence = "my name is kamal singh rawat"
-# p sentence.sub('kamal', 'deepak')
-
-# #variable assignment 
-# first_name = 'deepak'
-# new_first_name = first_name
-# p new_first_name
-
-# first_name = 'kamal'
-# p first_name
-# p new_first_name
-
-# #escaping
-# p "performing escaping using this arrow \#{first_name}"
-# #getting error
-# #p 'deepak 'singh rawat''
-# p'hey deepak, \'how u doing?\''
-
-# #get user input
-# # puts "whats your name"
-# # name = gets.chomp
-# # puts "u said ur name is #{name}"
- 
-# # puts "enter number"
-# # input = gets.chomp
-# # puts input.to_i * 2
-
-# # p "enter ur first name"
-# # first = gets.chomp
-# # p "enter ur last name"
-# # second = gets.chomp
-# # conn = first + ' ' + second
-# # rev = conn.reverse!
-# # p "your full name reversed is #{rev}"
-# # p rev.length
 
 # #numbers
 # p "i am a line"
@@ -87,7 +16,7 @@ p new_address
 # other = true
 # if condition || other
 #     p 'hello'
-# else 
+# else
 #     p 'bye'
 # end
 
@@ -101,19 +30,19 @@ p new_address
 # end
 # #------------------------------
 # def multiply(first_number, second_number)
-#   first_number.to_f * second_number.to_f      
+#   first_number.to_f * second_number.to_f
 # end
- 
+
 # def divide(first_number, second_number)
-#   first_number.to_f / second_number.to_f      
+#   first_number.to_f / second_number.to_f
 # end
- 
+
 # def subtract(first_number, second_number)
-#   second_number.to_f - first_number.to_f      
+#   second_number.to_f - first_number.to_f
 # end
- 
+
 # def mod(first_number, second_number)
-#   first_number.to_f % second_number.to_f      
+#   first_number.to_f % second_number.to_f
 # end
 
 # puts "What do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
@@ -142,57 +71,7 @@ p new_address
 #   puts "You have made an invalid choice"
 # end
 
-# #Array
-# a = [1,2,3,4,5]
-# p a.last
 
-# p "------------"
-# b = (1..10).to_a
-# # p b.shuffle!
-# p b.reverse!
-# p b.reverse
-# p b
-
-# p "------------"
-# x = "a".."z"
-# p x.to_a
-
-# p "------------"
-# p x.to_a.shuffle
-# p x.to_a.length
-
-# p "------------"
-# arr =  a << 10
-# p arr
-
-# p a.include?(1)
-# p a.append("deepak")
-# p a.pop()
-# p a
-
-# p "------------"
-# p a.join
-# p a.join('-')
-# p a.join(',')
-
-# para = %w(my name is deepak singh rawat)
-# p para
-
-# p "------------"
-# para.each do |food|
-#   print food + " "
-# end
-
-# p "------------"
-# #in same line
-# para.each {|food| print food.capitalize + " "}
-
-# p "------------"
-# #print only odd numbers
-# zz = (1..100).to_a.shuffle
-# p zz
-# p "------------"
-# # zz.select {|number| print number.odd?}
 
 # p "------------"
 # #hash
